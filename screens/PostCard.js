157458adf1147e939642b5f69daf590698c8ee88
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-12-10 11:00:08 -0600

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -48,7 +48,7 @@ export default class PostCard extends Component {
             image_7: require("../assets/image_7.jpg")
         };
         return (
-            <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate("PostScreen", post = this.props.post)}>
+            <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate("PantalladePublicación", post = this.props.post)}>
                 <View style={this.state.light_theme ? styles.cardContainerLight : styles.cardContainer}>
                     <View style={styles.authorContainer}>
                         <View style={styles.authorImageContainer}>
@@ -70,7 +70,7 @@ export default class PostCard extends Component {
                     <View style={styles.actionContainer}>
                         <View style={styles.likeButton}>
                             <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                            <Text style={styles.likeText}>12k</Text>
+                            <Text style={styles.likeText}>12m</Text>
                         </View>
                     </View>
                 </View>
